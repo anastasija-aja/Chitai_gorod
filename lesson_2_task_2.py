e9ec20714d9a8f6607163be46d1176bab9e1a3d7
anastasija-aja <viens2263@gmail.com>
2025-02-01 13:11:08 +0300

    lesson2

--- lesson_2_task_2.py
+++ lesson_2_task_2.py
@@ -1,10 +1,10 @@
-is_year_leap=input("Введите год:")
-is_year_leap=int(is_year_leap)
-if (is_year_leap % 4==0):
-    print("год",is_year_leap,":",True)
-if (is_year_leap % 4==1):
+is_year_leap = input("Введите год:")
+is_year_leap = int(is_year_leap)
+if is_year_leap % 4 == 0:
+    print("год", is_year_leap, ":", True)
+if is_year_leap % 4 == 1:
     print("год", is_year_leap, ":", False)
-if (is_year_leap % 4 == 2):
+if is_year_leap % 4 == 2:
     print("год", is_year_leap, ":", False)
-if (is_year_leap % 4 == 3):
+if is_year_leap % 4 == 3:
     print("год", is_year_leap, ":", False)
--- lesson_2_task_3.py
+++ lesson_2_task_3.py
@@ -2,6 +2,3 @@ side=input("Введите длину стороны:")
 side=float(side)
 import math
 print("side:", math.ceil(side)*math.ceil(side))
-
-
-
