60a0c2e006560e5de3cef7a3b203e162e854375e
Anastasija <viens2263@gmail.com>
2025-04-30 22:13:50 +0300

    Delete 02_lesson directory
    *

--- 02_lesson/lesson_2_task_5.py
+++
@@ -1,9 +0,0 @@
-month = int(input("Введите номер месяца (1-12):"))
-if month == 12 or month in range(1,2+1):
-    print("Зима")
-elif 3 <= month <= 5:
-    print("Весна")
-elif 6 <= month <= 8:
-    print("Лето")
-elif 9 <= month <= 11:
-    print("Осень")
