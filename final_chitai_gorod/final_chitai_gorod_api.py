b99c99897694d8c90817e49db579ef066bd8126f
anastasija-aja <viens2263@gmail.com>
2025-04-30 17:56:02 +0300

    final_chitai_gorod

--- final_chitai_gorod/final_chitai_gorod_api.py
+++ final_chitai_gorod/final_chitai_gorod_api.py
@@ -1,5 +1,4 @@
 import pytest
-import allure
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
